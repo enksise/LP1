30dfdd4dc6fb56152a7cb3ac1b4c44e0da7276f4
enksise <dudunakano@gmail.com>
2026-08-13 11:47:15 -0300

    Atualização do código
    Adicionado saída dos resultados e resumo do aluguel

--- Exercicio-de-JAVA/src/exercicio7.java
+++ Exercicio-de-JAVA/src/exercicio7.java
@@ -22,11 +22,20 @@ public class exercicio7 {
         System.out.println("Digite quantos km foram percorridos: ");
         km = s.nextDouble();
 
+        //Verifica se ultrapassou o limite de km
         if (km > limite) {
             adicional = adicional * (km - limite);
         }
 
+        //Calcula o aluguel e exibe na tela um resumo do aluguel
         aluguel = aluguel + adicional;
-        System.out.println("Total do aluguel: R$ ");
+        
+        System.out.println("==== RESUMO ===================");
+        System.out.println("Dias alugados: " + dias);
+        System.out.println("km percorridos: " + km);
+        System.out.printf("Diária: R$ %.2f%n", diaria);
+        System.out.printf("Adicional: R$ %.2f%n", adicional);
+        System.out.printf("Total do aluguel: R$ %.2f%n", aluguel);
+        System.out.println("===============================");
     }
 }
